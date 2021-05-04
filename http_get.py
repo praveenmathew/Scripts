acb075acf3c2054f0849b30e1cdc6dfdc9b6764d
praveenmathew <praveenkuttinal@gmail.com>
2021-05-04 12:52:20 +0800

    Update http_get.py

--- http_get.py
+++ http_get.py
@@ -1,6 +1,8 @@
 import requests
 
 URL = "http://ptl-e34d116d-c53b863a.libcurl.so/pentesterlab"
-cookies = dict(key='please')
-r = requests.get(url = URL, cookies = cookies)
+#cookies = dict(key='please')
+#r = requests.get(url = URL, cookies = cookies)
+headers = {'Accept-Language' : 'key-please'}
+r = requests.get(url = URL, headers = headers)
 print (r.content)
