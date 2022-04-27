c3102db0b06e862d07bd291a5bd6e7b4156fc0e8
praveenmathew <praveenkuttinal@gmail.com>
2022-04-27 11:12:01 +0800

    Create Virus_Total_Check.py

---
+++ Virus_Total_Check.py
@@ -0,0 +1,31 @@
+'''Reads a list of IP/domain from a source, checks their reputation in VirusTotal, and returns result
+'''
+
+import json
+import requests
+from pprint import pprint
+
+def is_ip(address):
+    return address.replace('.', '').isnumeric()
+
+new_list = []
+
+query =  "<Insert list of IP/Domain here>"
+for item in query:
+    if(is_ip(item) == True):
+        api_u = "https://www.virustotal.com/api/v3/ip_addresses/"
+        api_url = api_u + item
+    else:
+        api_ip = "https://www.virustotal.com/api/v3/domains/"
+        api_url = api_ip + item
+    header = {
+            "Accept":"application/json",
+            "x-apikey":"<Your API Key here>"
+    }
+    response = requests.request(method='GET',url=api_url,headers=header).json()
+    stats = response['data']['attributes']['last_analysis_stats']
+    new_list.append(stats)
+    stats['IoC'] = item
+    
+
+print(new_list)
